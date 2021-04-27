69587fae6cfa108d0a3fa7ad69faf9507590e09b
Alex Suciu <>
2021-04-27 09:19:32 +0000

    removed help comm

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ async def on_ready():
   print(f"{bot.user} is alive!")
 
   await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="p!info"))
+  bot.remove_command('help')
 
 # the oldest joke in the book
 @bot.command()
@@ -65,7 +66,7 @@ async def tenie(message):
     await message.send('Era odata un om si avea o tenie de care nu putea sa scape.Disperat, merge la cel mai bun doctor din lume, doctorul de tenii.Il intreaba pe doctor daca il poate ajuta, iar doctorul ii raspunde "Bine, vino maine cu o banana si un biscuite".Curios, omul nostru vine urmatoarea zi, doctorul il apleaca pe masa si ii baga in fund banana si dupa aceea biscuitele. "Foarte bine, repetam tratamentul 2 saptamani!". Disperat, omul face tratamentul si la sfarsit doctorul ii spune "Maine e ultima zi, sa vii cu o banana si un ciocan." Speriat, vine omul urmatoarea zi, il apleaca doctorul pe masa, ii baga banana in fund si asteapta... si asteapta... la care iese tenia "Si biscuitele meu unde e?" si doctorul ii da un ciocan in cap si moare.')
 
 # info command
-@bot.command()
+@bot.command(aliases = ["Info", "INFO"])
 async def info(message):
   t = "Bot by AlphaPuffer"
   emb = discord.Embed(title = t, description = "Here's a list of all of the commands:", color=discord.Colour.from_rgb(242, 235, 34))
@@ -266,14 +267,6 @@ async def dislike(message, nr = None):
     t = "Joke number not valid, please try again!"
     emb = discord.Embed(title = t, color=discord.Colour.from_rgb(242, 235, 34))
     await message.send(embed = emb)
-
-@bot.command()
-async def pr(message):
-  likes = db["likes"]
-  dislikes = db["dislikes"]
-  print(likes)
-  print(dislikes)
-  print("-----")
 
 web_server()
 bot.run(os.getenv('TOKEN'))
